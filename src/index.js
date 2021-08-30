b6f8e42082c9498757bf09d4aae5d03ea14f5e1b
CHRONO DEV <85164498+CHRONOS-DEVELOPER@users.noreply.github.com>
2021-08-30 13:45:21 -0300

    versão 1.0 finalizada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 window.screenmain = document.getElementById("screenmain")
 
 function configpage() {
-    screenmain.innerHTML = "<style>.btn-lg-lg{font-size:3vw;margin-top:5vh}</style><h1 class='textlogo' style='color:#999999;'> CONFIGURAÇÕES </h1><br><button class='btn btn-block btn-outline-success btn-lg-lg'type='button'>CONFIGURAÇÕES DE ADMIN</button><br><button class='btn btn-block btn-outline-success btn-lg-lg'type='button'>IMPRIMIR RELATÓRIO</button><br><button onclick='telaupload()'class='btn btn-block btn-outline-success btn-lg-lg'type='button'>IMPORTAR DADOS</button><br><button onclick='gerararquivo()'class='btn btn-block btn-outline-success btn-lg-lg'type='button'>EXPORTAR DADOS</button><br>"
+    screenmain.innerHTML = "<style>.btn-lg-lg{font-size:3vw;margin-top:5vh}</style><h1 class='textlogo' style='color:#999999;'> CONFIGURAÇÕES </h1><br><button onclick='configuraradminpage()' class='btn btn-block btn-outline-success btn-lg-lg'type='button'>CONFIGURAÇÕES DE ADMIN</button><br><button onclick='imprimir()'class='btn btn-block btn-outline-success btn-lg-lg'type='button'>IMPRIMIR RELATÓRIO</button><br><button onclick='telaupload()'class='btn btn-block btn-outline-success btn-lg-lg'type='button'>IMPORTAR DADOS</button><br><button onclick='gerararquivo()'class='btn btn-block btn-outline-success btn-lg-lg'type='button'>EXPORTAR DADOS</button><br>"
 }
 
 function cadastrarlivropage() {
@@ -113,4 +113,307 @@ function gerararquivo() {
     blob = new Blob([localStorage.DB_books], { type: "text/plain;charset=UTF-8" })
     saveAs(blob, "backupbiblioteca.json")
     configpage()
+}
+
+function acessaracervopage() {
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999;'>PROCURAR LIVRO</h1><br><input class='form-control form-control-lg' type='text' id='bookname' placeholder='NOME DO LIVRO'><br><button onclick='procurarlivro()' class='btn btn-block btn-outline-success btn-lg'type='button'>BUSCAR LIVRO</button><br><button onclick='listarlivros()' class='btn btn-block btn-outline-success btn-lg'type='button'>LISTA COMPLETA</button>"
+    document.getElementById("bookname").focus();
+    document.getElementById("bookname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            procurarlivro();
+
+        }
+    })
+
+}
+
+function procurarlivro() {
+    bookname = document.getElementById("bookname").value;
+    booklist = []
+
+    if (bookname != "") {
+        DB_books = JSON.parse(localStorage.DB_books)
+        screenmain.innerHTML = "<div style=' max-height:80vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div><button onclick='acessaracervopage()' class='btn btn-block btn-outline-success btn-lg'type='button'>VOLTAR</button>"
+        n = 1
+        DB_books.forEach(element => {
+            if (element.bookname == bookname) {
+                table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+                $("#tabeladelivros").append(table);
+                if (element.status == "disponível") {
+                    statusstyle = document.getElementById(n).classList.add("text-success")
+                } else {
+                    statusstyle = document.getElementById(n).classList.add("text-danger")
+                }
+
+
+                n++;
+            }
+
+        })
+
+    }
+}
+
+function listarlivros() {
+    bookname = document.getElementById("bookname").value;
+    booklist = []
+
+
+    DB_books = JSON.parse(localStorage.DB_books)
+    screenmain.innerHTML = "<div style=' max-height:80vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div><button onclick='acessaracervopage()' class='btn btn-block btn-outline-success btn-lg'type='button'>VOLTAR</button>"
+    n = 1
+    DB_books.forEach(element => {
+
+        table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+        $("#tabeladelivros").append(table);
+        if (element.status == "disponível") {
+            statusstyle = document.getElementById(n).classList.add("text-success")
+        } else {
+            statusstyle = document.getElementById(n).classList.add("text-danger")
+        }
+
+
+        n++;
+
+
+    })
+
+}
+
+function homepage() {
+    if (localStorage.DB_books) {
+        DB_books = JSON.parse(localStorage.DB_books)
+
+        screenmain.innerHTML = "<h1 class='textlogohome' style='color:#999999;'> LIVROS INDISPOÍVEIS </h1><div style=' max-height:95vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div>"
+        n = 1
+        DB_books.forEach(element => {
+            if (element.status == "indisponível") {
+                table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+                $("#tabeladelivros").append(table);
+                statusstyle = document.getElementById(n).classList.add("text-danger")
+                n++;
+            }
+
+        })
+        if (n == 1) {
+            screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999; margin: 50vh 0'> NENHUM LIVRO INDISPONÍVEL</h1>"
+        }
+    } else {
+        screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999; margin: 50vh 0'> CADASTRE OS LIVROS</h1>"
+    }
+}
+
+function emprestarlivropage() {
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999;'>PROCURAR LIVRO EMPRESTIMO</h1><br><input class='form-control form-control-lg' type='text' id='bookname' placeholder='NOME DO LIVRO'><input class='form-control form-control-lg' type='text' id='ALUNOname' placeholder='NOME ALUNO'><br><button onclick='emprestimodelivro()' class='btn btn-block btn-outline-success btn-lg'type='button'>BUSCAR LIVRO</button>"
+    document.getElementById("bookname").focus();
+    document.getElementById("bookname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            document.getElementById("ALUNOname").focus()
+
+        }
+    })
+    document.getElementById("ALUNOname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            emprestimodelivro();
+
+        }
+    })
+}
+
+function emprestimodelivro() {
+    bookname = document.getElementById("bookname").value;
+    window.alunoname = document.getElementById("ALUNOname").value
+    booklist = []
+
+    if (bookname != "" && alunoname != "") {
+        DB_books = JSON.parse(localStorage.DB_books)
+        window.idlivro = bookname
+        screenmain.innerHTML = "<div style=' max-height:80vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div><button id='buttonenter' onclick='emprestar()'class='btn btn-block btn-outline-success btn-lg'type='button'>EMPRESTAR</button><br><button onclick='emprestarlivropage()'class='btn btn-block btn-outline-success btn-lg'type='button'>VOLTAR</button>"
+        n = 1
+        DB_books.forEach(element => {
+            if (element.bookname == bookname) {
+                table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+                $("#tabeladelivros").append(table);
+                if (element.status == "disponível") {
+                    statusstyle = document.getElementById(n).classList.add("text-success")
+                } else {
+                    statusstyle = document.getElementById(n).classList.add("text-danger")
+                }
+
+
+                n++;
+            }
+
+        })
+
+    }
+
+}
+
+function emprestar() {
+    DB_books = JSON.parse(localStorage.DB_books)
+
+    DB_books.every((element, res) => {
+        if (element.bookname == idlivro && element.status == "disponível") {
+            DB_books[res].status = "indisponível"
+            DB_books[res].aluno = alunoname;
+            data = new Date();
+            dia = data.getDate();
+            mes = data.getMonth() + 1;
+            ano = data.getFullYear();
+            DB_books[res].datadeemprestimo = dia + "/" + mes + "/" + ano
+            localStorage.DB_books = JSON.stringify(DB_books)
+            return false
+        } else {
+            return true
+        }
+
+    });
+    homepage()
+}
+
+function devolverlivropage() {
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999;'>PROCURAR LIVRO DEVOLUÇÃO</h1><br><input class='form-control form-control-lg' type='text' id='bookname' placeholder='NOME DO LIVRO'><input class='form-control form-control-lg' type='text' id='ALUNOname' placeholder='NOME ALUNO'><br><button onclick='devoluçãodelivro()' class='btn btn-block btn-outline-success btn-lg'type='button'>BUSCAR LIVRO</button>"
+    document.getElementById("bookname").focus();
+    document.getElementById("bookname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            document.getElementById("ALUNOname").focus()
+
+        }
+    })
+    document.getElementById("ALUNOname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            devoluçãodelivro();
+
+        }
+    })
+}
+
+function devoluçãodelivro() {
+    bookname = document.getElementById("bookname").value;
+    window.alunoname = document.getElementById("ALUNOname").value
+    booklist = []
+
+    if (bookname != "" && alunoname != "") {
+        DB_books = JSON.parse(localStorage.DB_books)
+        window.idlivro = bookname
+        screenmain.innerHTML = "<div style=' max-height:80vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div><button id='buttonenter' onclick='devolver()'class='btn btn-block btn-outline-success btn-lg'type='button'>DEVOLVER</button><br><button onclick='devolverlivropage()'class='btn btn-block btn-outline-success btn-lg'type='button'>VOLTAR</button>"
+        n = 1
+        DB_books.forEach(element => {
+            if (element.bookname == bookname && element.aluno == alunoname) {
+                table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+                $("#tabeladelivros").append(table);
+                statusstyle = document.getElementById(n).classList.add("text-danger")
+                n++;
+            }
+
+        })
+
+    }
+}
+
+function devolver() {
+    DB_books = JSON.parse(localStorage.DB_books)
+
+    DB_books.every((element, res) => {
+        if (element.bookname == idlivro && element.status == "indisponível" && element.aluno == alunoname) {
+            DB_books[res].status = "disponível"
+            DB_books[res].aluno = "livre";
+
+            DB_books[res].datadeemprestimo = null
+            localStorage.DB_books = JSON.stringify(DB_books)
+            return false
+        } else {
+            return true
+        }
+
+    });
+    homepage()
+}
+
+function pesquisarfichapage() {
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999;'>PROCURAR ALUNO</h1><br><input class='form-control form-control-lg' type='text' id='ALUNOname' placeholder='NOME ALUNO'><br><button onclick='pesquisarficha()' class='btn btn-block btn-outline-success btn-lg'type='button'>BUSCAR ALUNO</button>"
+    document.getElementById("ALUNOname").focus();
+    document.getElementById("ALUNOname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            pesquisarficha()
+
+        }
+    })
+}
+
+function pesquisarficha() {
+    window.alunoname = document.getElementById("ALUNOname").value
+    if (alunoname != "") {
+        DB_books = JSON.parse(localStorage.DB_books)
+        window.idlivro = bookname
+        screenmain.innerHTML = "<div style=' max-height:80vh; overflow-y:scroll;width:70vw; margin:3vh 2vw;'><table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table><br></div><br><button onclick='pesquisarfichapage()'class='btn btn-block btn-outline-success btn-lg'type='button'>VOLTAR</button>"
+        n = 1
+        DB_books.forEach(element => {
+            if (element.aluno == alunoname) {
+                table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+                $("#tabeladelivros").append(table);
+                statusstyle = document.getElementById(n).classList.add("text-danger")
+                n++;
+            }
+
+        })
+
+    }
+}
+
+function configuraradminpage() {
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999;'>CONFIGURAÇÕES DE ADMINISTRADOR</h1><br><input class='form-control form-control-lg' type='text' id='adminname' placeholder='NOME DO ADMINISTRADOR'><input class='form-control form-control-lg' type='text' id='adminpass' placeholder='SENHA DO ADMINISTRADOR'><br><button onclick='alterardados()' class='btn btn-block btn-outline-success btn-lg'type='button'>ATUALIZAR DADOS</button>"
+    document.getElementById("adminname").focus();
+    document.getElementById("adminname").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            document.getElementById("adminpass").focus()
+
+        }
+    })
+    document.getElementById("adminpass").addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            alterardados();
+
+        }
+    })
+}
+
+function alterardados() {
+    admindb = JSON.parse(localStorage.AdminUser)
+    admindb.user = document.getElementById("adminname").value;
+    admindb.password = document.getElementById("adminpass").value;
+    localStorage.AdminUser = JSON.stringify(admindb);
+    screenmain.innerHTML = "<h1 class='textlogo' style='color:#999999; margin: 50vh 0'> DADOS ALTERADOS COM SUCESSO!!</h1>"
+    var delayInMilliseconds = 3000;
+
+    setTimeout(function() {
+        homepage()
+    }, delayInMilliseconds);
+
+}
+
+function imprimir() {
+    body = document.getElementById("body")
+    DB_books = JSON.parse(localStorage.DB_books)
+    body.innerHTML = "<table id='tabeladelivros' class='table table-striped table-light'><thead><tr><th scope='col'>NOME DO LIVRO</th><th scope='col'>GÊNERO DO LIVRO</th><th scope='col'>STATUS</th><th scope='col'>ALUNO</th><th scope='col'>DATA DE EMPRÉSTIMO</th></tr></thead><tbody></tbody></table>"
+    n = 1
+    DB_books.forEach(element => {
+        if (element.status == "indisponível") {
+            table = "<tr><th scope='col'>" + element.bookname + "</th><th scope='col'>" + element.bookgender + "</th><th id='" + n + "' scope='col'>" + element.status + "</th><th id='" + n + "'scope='col'>" + element.aluno + "</th><th scope='col'>" + element.datadeemprestimo + "</th></tr>"
+            $("#tabeladelivros").append(table);
+            if (element.status == "disponível") {
+                statusstyle = document.getElementById(n).classList.add("text-success")
+            } else {
+                statusstyle = document.getElementById(n).classList.add("text-danger")
+            }
+
+
+            n++;
+
+        }
+    })
+    window.print();
+    window.location.assign("index.html")
+
 }
